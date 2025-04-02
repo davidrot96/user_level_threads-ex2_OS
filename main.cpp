974428140d19f6d19a1134139d443664ab7714cc
david rotenberg <david@davidrot27@gmail.com>
2025-04-02 13:43:09 +0300

    Add main file to test the program
    to build the all lib with main.cpp file just run make
    from the main folder.
    you can then run ./main.

--- main.cpp
+++ main.cpp
@@ -14,23 +14,6 @@ void thread_function(void) {
     }
 }
 
-void f1() {
-    int tid = uthread_get_tid();
-    std::cout << "f1 is runnint - tid is: " << tid << "\n";
-    sleep(1);
-}
-
-void f2() {
-    int tid = uthread_get_tid();
-    std::cout << "f2 is runnint - tid is: " << tid << "\n";
-    sleep(1);
-}
-
-void f3() {
-    int tid = uthread_get_tid();
-    std::cout << "f3 is runnint - tid is: " << tid << "\n";
-    sleep(1);
-}
 
 int main() {
     if (uthread_init(QUANTUM_USECS) == -1) {
@@ -45,34 +28,34 @@ int main() {
     std::cout << "f2_tid is: " << f2_tid << std::endl;
     std::cout << "f3_tid is: " << f3_tid << std::endl;
 
-    // int tids[NUM_THREADS];
-    // for (int i = 0; i < NUM_THREADS; ++i) {
-    //     tids[i] = uthread_spawn(thread_function);
-    //     if (tids[i] == -1) {
-    //         std::cerr << "uthread_spawn failed for thread " << i << std::endl;
-    //         return 1;
-    //     }
-    // }
-    // // tids[NUM_THREADS] = uthread_spawn(printer);
-    // for (int i = 0; i < NUM_THREADS; ++i) {
-    //     std::cout << tids[i] << std::endl;
-    // }
+    int tids[NUM_THREADS];
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        tids[i] = uthread_spawn(thread_function);
+        if (tids[i] == -1) {
+            std::cerr << "uthread_spawn failed for thread " << i << std::endl;
+            return 1;
+        }
+    }
+    // tids[NUM_THREADS] = uthread_spawn(printer);
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        std::cout << tids[i] << std::endl;
+    }
 
-    // // Main thread also does some work
-    // for (int i = 0; i < 3; ++i) {
-    //     std::cout << "Main thread is running. Quantum " << uthread_get_quantums(0) << std::endl;
-    //     sleep(1);
-    // }
-    // sleep(5);
+    // Main thread also does some work
+    for (int i = 0; i < 3; ++i) {
+        std::cout << "Main thread is running. Quantum " << uthread_get_quantums(0) << std::endl;
+        sleep(1);
+    }
+    sleep(5);
     
 
-    // // Terminate all threads
-    // for (int i = 0; i < NUM_THREADS; ++i) {
-    //     uthread_terminate(tids[i]);
-    // }
-    // uthread_terminate(0); // Terminate main thread.
+    // Terminate all threads
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        uthread_terminate(tids[i]);
+    }
+    uthread_terminate(0); // Terminate main thread.
 
-    // std::cout << "Total quantums: " << uthread_get_total_quantums() << std::endl;
+    std::cout << "Total quantums: " << uthread_get_total_quantums() << std::endl;
 
     return 0;
 }
